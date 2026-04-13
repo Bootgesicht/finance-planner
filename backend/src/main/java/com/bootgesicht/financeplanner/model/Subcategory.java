a9b1dc43ec6d4d88e66234393f46614111058e09
Bootgesicht <jonas.hockmann@gmail.com>
2026-04-13 20:59:50 +0200

    Add Subcategory model

--- backend/src/main/java/com/bootgesicht/financeplanner/model/Subcategory.java
+++ backend/src/main/java/com/bootgesicht/financeplanner/model/Subcategory.java
@@ -1,5 +1,37 @@
 package com.bootgesicht.financeplanner.model;
 
 public class Subcategory {
-    
+    private int id;
+    private int categoryId;
+    private String name;
+
+    public Subcategory(int id, int categoryId, String name) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
