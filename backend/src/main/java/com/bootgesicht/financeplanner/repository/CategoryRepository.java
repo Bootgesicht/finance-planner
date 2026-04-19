aa22f911558e93e06f01d59e94b745a582937ac6
Bootgesicht <jonas.ristau.jr@gmail.com>
2026-04-19 20:48:58 +0200

    Add CategoryRepository with basic CRUD

--- backend/src/main/java/com/bootgesicht/financeplanner/repository/CategoryRepository.java
+++ backend/src/main/java/com/bootgesicht/financeplanner/repository/CategoryRepository.java
@@ -1,5 +1,123 @@
 package com.bootgesicht.financeplanner.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bootgesicht.financeplanner.model.Category;
+import com.bootgesicht.financeplanner.model.CategoryKind;
+
 public class CategoryRepository {
-    
+
+    public List<Category> findAll() {
+        List<Category> categories = new ArrayList<>();
+
+        String sql = """
+                SELECT id, name, kind
+                FROM categories
+                        """;
+
+        try (
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();) {
+
+            while (rs.next()) {
+                Category category = new Category(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        CategoryKind.valueOf(rs.getString("kind")));
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
+
+    public Category findByName(String name) {
+        String sql = """
+                SELECT id, name, kind
+                FROM categories
+                WHERE name = ?
+                        """;
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setString(1, name);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Category(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            CategoryKind.valueOf(rs.getString("kind")));
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Category findByID(int id) {
+        String sql = """
+                SELECT id, name, kind
+                FROM categories
+                WHERE id = ?
+                        """;
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Category(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            CategoryKind.valueOf(rs.getString("kind")));
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void save(Category category) {
+        String sql = """
+                INSERT INTO categories (name, kind)
+                VALUES (?, ?)
+                """;
+        try (
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setString(1, category.getCategoryName());
+            ps.setString(2, category.getCategoryKind().name());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteById(int id) {
+        String sql = """
+                DELETE FROM categories
+                WHERE id = ?
+                """;
+
+        try (
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
